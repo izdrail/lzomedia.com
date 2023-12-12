79a2377bbb88ff5700ae7e93487b70a1663f42f2
Stefan <bogdan.izdrail@gmail.com>
2023-12-12 17:56:27 +0000

    Fixed some seo issues

--- src/vue/SeoAnalyzer.vue
+++ src/vue/SeoAnalyzer.vue
@@ -1,5 +1,107 @@
+
+<script>
+console.log('seo-analyzer.vue')
+export default {
+    name: 'SeoAnalyzer',
+    data() {
+        return {
+            title: 'SEO Analyzer',
+            subtitle: 'Get a more detail SEO report for your website by contacting me directly or setup a meeting.',
+            loading: false,
+            link: '',
+            response: '',
+            display_errors: false,
+            errors: '',
+            // SEO Report
+            render_report: false,
+            loading_time: '',
+            word_count: '',
+            warnings: [],
+            keywords: [],
+            headings: [],
+            // Charts
+        }
+    },
+    methods: {
+        getSeoReport() {
+            // Show loading icon
+            this.loading = true;
+            // Make API call
+            fetch(`https://api.todayintel.com/seo/analyze`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    link: this.link,
+                    format: 'json',
+                }),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    this.loading = false;
+                    this.render_report = true;
+                    this.response = data;
+                    this.loading_time = data.data.total_time;
+                    this.word_count = data.data.pages[0].word_count;
+                    this.warnings = data.data.pages[0].warnings;
+                    this.keywords = data.data.pages[0].keywords;
+                    this.headings = data.data.pages[0].headings;
+
+                })
+                .catch(error => {
+                    console.log(error);
+                    this.display_errors = true;
+
+                    this.errors = error;
+                    this.loading = false;
+                });
+        },
+    },
+    mounted() {
+        console.log('Component mounted.')
+    },
+}
+
+</script>
+<style>
+
+.pill {
+    display: flex;
+    padding: 0.5rem 1rem;
+    gap: 0.5rem;
+    color: var(--accent-text-over);
+    border: 1px solid var(--accent-regular);
+    background-color: var(--accent-regular);
+    border-radius: 999rem;
+    font-size: var(--text-md);
+    line-height: 1.35;
+    white-space: nowrap;
+}
+</style>
+
 <template>
   <section>
+      <!-- THE Errors!-->
+      <div class="w-full max-w-sm mx-auto mt-6">
+        <div v-if="display_errors" class="flex w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+              <div class="flex items-center justify-center w-12 bg-red-500">
+                  <svg class="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z" />
+                  </svg>
+              </div>
+
+              <div class="px-4 py-2 -mx-3">
+                  <div class="mx-3">
+                      <span class="font-semibold text-red-500 dark:text-red-400">Error</span>
+                      <p class="text-sm text-gray-600 dark:text-gray-200">
+                          {{ errors }}
+                      </p>
+                  </div>
+              </div>
+          </div>
+    </div>
     <!-- THE FORM !-->
     <div class="container px-6 py-16 mx-auto text-center">
       <div class="max-w-lg mx-auto">
@@ -23,26 +125,6 @@
           <p class="text-sm text-gray-600 mt-2  dark:text-gray-400">
            Please provide a valid URL.
           </p>
-        </div>
-      </div>
-
-      <div class="w-full max-w-sm mx-auto mt-6">
-        <div v-if="display_errors" class="flex w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
-          <div class="flex items-center justify-center w-12 bg-red-500">
-            <svg class="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
-              <path d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z" />
-            </svg>
-          </div>
-
-          <div class="px-4 py-2 -mx-3">
-            <div class="mx-3">
-              <span class="font-semibold text-red-500 dark:text-red-400">Error</span>
-              <p class="text-sm text-gray-600 dark:text-gray-200">
-                {{ errors }}
-              </p>
-            </div>
-          </div>
-
         </div>
       </div>
     </div>
@@ -187,94 +269,5 @@
 
     </div>
 
-
-
   </section>
 </template>
-
-
-
-<script>
-console.log('seo-analyzer.vue')
-
-
-export default {
-  name: 'SeoAnalyzer',
-
-  data() {
-    return {
-      title: 'SEO Analyzer',
-      subtitle: 'Get a more detail SEO report for your website by contacting me directly or setup a meeting.',
-      loading: false,
-      link: '',
-      response: '',
-      display_errors: false,
-      errors: '',
-      // SEO Report
-      render_report: false,
-      loading_time: '',
-      word_count: '',
-      warnings: [],
-      keywords: [],
-      headings: [],
-      // Charts
-    }
-  },
-  methods: {
-    getSeoReport() {
-      // Show loading icon
-      this.loading = true;
-      // Make API call
-      fetch(`https://api.todayintel.com/seo/analyze`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          link: this.link,
-          format: 'json',
-        }),
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
-          this.loading = false;
-          this.render_report = true;
-          this.response = data;
-          this.loading_time = data.data.total_time;
-          this.word_count = data.data.pages[0].word_count;
-          this.warnings = data.data.pages[0].warnings;
-          this.keywords = data.data.pages[0].keywords;
-          this.headings = data.data.pages[0].headings;
-
-        })
-        .catch(error => {
-          console.log(error);
-          this.display_errors = true;
-
-          this.errors = error;
-          this.loading = false;
-        });
-    },
-  },
-  mounted() {
-    console.log('Component mounted.')
-  },
-}
-
-</script>
-<style>
-
-.pill {
-  display: flex;
-  padding: 0.5rem 1rem;
-  gap: 0.5rem;
-  color: var(--accent-text-over);
-  border: 1px solid var(--accent-regular);
-  background-color: var(--accent-regular);
-  border-radius: 999rem;
-  font-size: var(--text-md);
-  line-height: 1.35;
-  white-space: nowrap;
-}
-</style>
