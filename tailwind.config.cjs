8840cc01edf7d83aff396f4b184f6494f4468357
Stefan <stefan@lzomedia.com>
2024-02-15 11:30:36 +0000

    Added flowbite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+        './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+        './node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
+        './node_modules/flowbite/**/*.{js,jsx,ts,tsx}'
+    ],
 
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+        require('flowbite/plugin')
+    ],
 }
