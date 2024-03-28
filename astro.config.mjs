94bdf815dce59445dccc5187d89252e0bf4af2eb
Stefan <stefan@lzomedia.com>
2024-03-28 08:33:34 +0000

    Bumped up to astro v4 and added pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 
-// https://astro.build/config
-
 import AstroPWA from '@vite-pwa/astro'
 
 export default defineConfig({
@@ -20,33 +18,6 @@ export default defineConfig({
       ],
     },
   },
-  plugins: [
-    {
-      name: 'vite-plugin-pwa',
-      options: {
-        includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
-        manifest: {
-          name: 'Senior Software Developer London',
-          short_name: 'Senior Software Developer London',
-          description: 'Senior Software Developer London available for hire in London',
-          theme_color: '#7611a6',
-          background_color: '#ffffff',
-          icons: [
-            {
-              src: 'favicon.svg',
-              sizes: '192x192',
-              type: 'image/svg+xml',
-            },
-            {
-              src: 'apple-touch-icon.png',
-              sizes: '180x180',
-              type: 'image/png',
-            },
-          ],
-        },
-      },
-    },
-  ],
   vite: {
     ssr: {
       noExternal: [
