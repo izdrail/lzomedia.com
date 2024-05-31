d6caf374f594e89479fb0b1777c70fdbfbe47d22
Stefan ♥ <stefan@lzomedia.com>
2024-05-31 16:16:38 +0100

    Added the email generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 
-import AstroPWA from '@vite-pwa/astro'
-
 export default defineConfig({
   integrations: [tailwind(),vue()],
   buildOptions: {
     outdir: 'dist',
     emptyOutDir: true,
-    sourcemap: true,
+    sourcemap: false,
     minify: true,
     rollupOptions: {
       plugins: [
